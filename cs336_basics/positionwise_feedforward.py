471447f5ca447927503f5e42f149def69b0fb6e5
regreeblu <chenyu_zhao@outlook.com>
2026-06-26 21:17:15 +0800

    Remove .d_model from SwiGLU class

--- cs336_basics/positionwise_feedforward.py
+++ cs336_basics/positionwise_feedforward.py
@@ -7,10 +7,10 @@ class SwiGLU(nn.Module):
         super().__init__()
         if d_ff is None:
             d_ff = int((8/3*d_model)//64*64)
-        self.d_model = d_model
         self.w1 = Linear(d_model, d_ff, device, dtype)
         self.w2 = Linear(d_ff, d_model, device, dtype)
         self.w3 = Linear(d_model, d_ff, device, dtype)
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         y1 = self.w1(x)
         silu_y1 = y1*torch.sigmoid(y1)

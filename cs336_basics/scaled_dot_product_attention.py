f6c3c1de50193a4270d27c86bcf97582e1fb2f12
regreeblu <chenyu_zhao@outlook.com>
2026-06-28 22:08:01 +0800

    Rename softmax parameter (dim_index to dim)

--- cs336_basics/scaled_dot_product_attention.py
+++ cs336_basics/scaled_dot_product_attention.py
@@ -7,5 +7,5 @@ def scaled_dot_product_attention(Q, K, V, mask=None):
     dot_product = torch.matmul(Q, K_trans)/(d_k ** 0.5)
     if mask is not None:
         dot_product = dot_product.masked_fill(~mask, float('-inf'))
-    attention = torch.matmul(softmax(dot_product, dim_index=-1), V)
+    attention = torch.matmul(softmax(dot_product, dim=-1), V)
     return attention
--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -1,9 +1,9 @@
 import torch
 
-def softmax(x: torch.Tensor, dim_index: int):
-    value_max = torch.max(x, dim=dim_index, keepdim=True).values
+def softmax(x: torch.Tensor, dim: int):
+    value_max = torch.max(x, dim=dim, keepdim=True).values
     exp_value = torch.exp(x - value_max)
-    sum_exp_value = torch.sum(exp_value, dim=dim_index, keepdim=True)
+    sum_exp_value = torch.sum(exp_value, dim=dim, keepdim=True)
     return exp_value/sum_exp_value
 
 
